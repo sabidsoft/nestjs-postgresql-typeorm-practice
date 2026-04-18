f26ae7ccbf0cb7caa3a80a2215ac8a8c10f19c83
Sabid Hasan <sabid.soft@gmail.com>
2026-04-18 23:15:28 +0600

    connect supabase postgresql with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URI'),
+        autoLoadEntities: true,
+        synchronize: true,
+      })
+    }),
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/users/users.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([User])
+    ]
+})
+export class UsersModule { }
